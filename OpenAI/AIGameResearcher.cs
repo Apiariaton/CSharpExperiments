3d19b74ffa8a0082286d1c9bd651521bd814bbcb
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2024-02-14 19:31:24 +0000

    Begin creating AIGameResearcher class

---
+++ OpenAI/AIGameResearcher.cs
@@ -0,0 +1,21 @@
+public class AIGameResearcher 
+{
+
+    public AIGameResearcher(string BoardGameName)
+    {
+        string boardGameName = BoardGameName;
+        bool boardGameExists = false;
+        string boardGameDescription = "";
+        int boardGamePrice = 0;
+    }
+
+
+
+
+
+
+
+
+
+
+}
