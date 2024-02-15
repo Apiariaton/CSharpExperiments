715643ee81cb62141ea77ee0babd53bf6feb9216
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2024-02-15 20:27:16 +0000

    Create basic starting structure and methods for AIGameResearcher class

--- OpenAI/AIGameResearcher.cs
+++ OpenAI/AIGameResearcher.cs
@@ -1,21 +1,69 @@
+namespace CSharpExperiments
+{
+
 public class AIGameResearcher 
 {
 
     public AIGameResearcher(string BoardGameName)
     {
         string boardGameName = BoardGameName;
-        bool boardGameExists = false;
-        string boardGameDescription = "";
-        int boardGamePrice = 0;
+        OpenAIQueryHandler openAIQueryHandler = new OpenAIQueryHandler(BoardGameName);
+    }
+
+
+
+    public async Task<GameResearchDto<GameResearchDto>> TryToObtainGameResearchObject()
+    {
+        var gameResearchObject = await openAIQueryHandler.ResearchGame();
+        var gameExists = gameResearchObject["gameExists"];
+        if (gameExists){
+            return gameResearchObject;
+        }
+        else
+        {
+            throw Exception("No game was found to exist with this title...");
+        }
+    }
+
+
+    public async Task<GameResearchDto<GameResearchDto>> ObtainGameResearchObject()
+    {
+        try
+        {
+            var gameResearchObject = await TryToObtainGameResearchObject();
+            return gameResearchObject;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+            throw exception;
+        }
+    }
+
+    public async Task<GameResearchDto<GameResearchDto>> TryToStoreGameResearchObject()
+    {
+      
+
+
+    }
+
+
+    public async Task<GameResearchDto<GameResearchDto>> StoreGameResearchObject()
+    {
+        try
+        {
+            await TryToStoreGameResearchObject();
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+            throw exception;
+        }
     }
 
 
 
 
 
-
-
-
-
-
+}
 }
