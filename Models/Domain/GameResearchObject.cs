e95c20ae53615cbdd9a92b10dd5415f3c4f05228
Apiariaton <109805867+Apiariaton@users.noreply.github.com>
2024-02-16 13:01:18 +0000

    Arrange folder structure for Models and OpenAI folders

---
+++ Models/Domain/GameResearchObject.cs
@@ -0,0 +1,23 @@
+namespace CSharpExperiments.API.Models.Domain
+{
+
+
+
+    public class GameResearchObject
+    {
+        public string boardGameName { get; set;}
+
+        public string boardGameDescription {get; set;}
+
+        public int boardGamePrice {get; set;}
+
+        public string boardGameUrl {get;set;}
+
+        public string boardGameGenre {get;set;}
+
+    }
+
+
+
+
+}
---
+++ OpenAI/OpenAIQueryHandler.cs
@@ -0,0 +1,39 @@
+using CSharpExperiments.API.Models.DTOs;
+
+namespace CSharpExperiments
+{
+
+    public class OpenAIQueryHandler {
+
+    public OpenAIQueryHandler(string BoardGameName)
+    {
+        public string boardGameName = BoardGameName;
+    }
+
+
+    public Task<GameResearchDto> ResearchGame()
+    {
+
+
+
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }   
+
+
+
+
+}
